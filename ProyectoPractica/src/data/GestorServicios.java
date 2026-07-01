476b02464b65a7410022684061f9cfa7c153f0bc
Gabriel Alejandro Gomez Pizarro <gabrielgomezpizarro0@gmail.com>
2026-07-01 08:47:33 -0400

    Update GestorServicios.java

--- ProyectoPractica/src/data/GestorServicios.java
+++ ProyectoPractica/src/data/GestorServicios.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorServicios {
-    public void instanciacion() {
+    public void cargarServicios() {
 
         List<ServicioTuristico> servicios = new ArrayList<>();
 
